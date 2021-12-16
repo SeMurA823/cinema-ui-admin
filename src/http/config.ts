9471161776192cb8974c141a6289ab7d855bdd7e
semura823 <muravevartem@outlook.com>
2021-12-16 21:07:41 +0400

    fix side effect

--- src/http/config.ts
+++ src/http/config.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
-import {AuthResponse} from "../models/response/AuthResponse";
+import AuthService from "../services/AuthService";
 
 export const SERVER_URL = "http://localhost:8080"
 
 export const API_URL = `${SERVER_URL}/api`
+
+let refreshing: boolean = false;
+let refreshRequest: Promise<any> | null = null;
 
 const $api = axios.create({
     withCredentials: true,
@@ -19,16 +22,28 @@ $api.interceptors.request.use((config) => {
     return config;
 })
 
+
 $api.interceptors.response.use((config) => {
     return config;
 }, async (error) => {
+    console.log("ERROR");
     const originalRequest = error.config;
-    if ((error.response.status === 401 || error.response.status === 403) && error.config && !error.config._isRetry) {
-        originalRequest._isRetry = true;
-        const response = await $api.post<AuthResponse>(`/auth?refresh`)
-        localStorage.setItem('token', response.data.accessToken);
-        return $api.request(originalRequest)
+    if (error.response.status === 401 || error.response.status === 403) {
+        if (error.config && !error.config._isRetry && !refreshing) {
+            refreshing = true;
+            refreshRequest = AuthService.refresh();
+            const response = await refreshRequest;
+            localStorage.setItem('token', response.data.accessToken);
+            originalRequest._isRetry = true;
+            return $api.request(originalRequest);
+        }
+        if (refreshing) {
+            await refreshRequest;
+            return $api.request(originalRequest);
+        }
     }
+
+    throw error;
 })
 
 export default $api;
